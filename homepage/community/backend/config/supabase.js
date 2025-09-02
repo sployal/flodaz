80b13abb61f19332c46e498ac368eaf5f1f8a7c4
Davie <muigaid91@gmail.com>
2025-09-02 10:19:49 +0300

    added a local backend

---
+++ homepage/community/backend/config/supabase.js
@@ -0,0 +1,10 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+
+module.exports = { supabase, supabaseAdmin };
